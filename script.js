ef60afbe0725aad1546dcb03779b5280e9a3df8a
iphone13 <saeedhamdan360x@gmail.com>
2026-01-31 22:36:42 +0000

    fetch

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let watchlist = [];
 document.getElementById('search-button').addEventListener('click', searchMovies);
 document.getElementById('toggle-theme').addEventListener('click', toggleTheme);
 
+function searchMovies() {
+    const query = document.getElementById('search-input').value;
+    const url = `https://www.omdbapi.com/?apikey=${apiKey}&s=${query}`;
+    document.getElementById('movie-results').innerHTML = 'Searching...';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            if (data.Response === 'True') {
+                displayMovies(data.Search);
+            } else {
+                document.getElementById('movie-results').innerHTML = 'No movies found.';
+            }
+        })
+        .catch(error => {
+            document.getElementById('movie-results').innerHTML = 'Error fetching data.';
+        });
+}
+
