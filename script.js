b6850d270cce6302f73146da90f506cd13d76bc7
iphone13 <saeedhamdan360x@gmail.com>
2026-01-31 22:35:38 +0000

    add event listener

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const apiKey = '4896736f';
 let watchlist = [];
 
+document.getElementById('search-button').addEventListener('click', searchMovies);
+document.getElementById('toggle-theme').addEventListener('click', toggleTheme);
+
